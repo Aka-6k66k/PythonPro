6307e65244c9218f0f915c245d4294e7694b0a1d
kaka6k66kjie <2357006254@qq.com>
2019-11-03 16:03:18 +0800

    这是第一个Python代码

---
+++ demo.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Time:2019/11/3 16:00
+# @Author:wangjie
+# @File:demo.py
+# @Software: PyCharm
+def Aka():
+    for i in range(0,100):
+        if i % 2 == 1:
+            print(i)
+        else:
+            pass
+
+if __name__ == '__main__':
+    Aka()
